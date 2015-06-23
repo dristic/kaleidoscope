24c0d99e369a234ecec496bda9f9f2c21fd45f1b
Dan Ristic <dristic101@gmail.com>
2015-06-23 00:21:23 -0700

    Added complete AST parsing

--- compiler.cpp
+++ compiler.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+#include <map>
 
 // //////////////////////
 // Parser
@@ -114,7 +115,268 @@ class CallExprAST : public ExprAST {
       : Callee(callee), Args(args) {}
 };
 
+// PrototypeAST - This class represents the "prototype" for a function,
+// which captures its name, and argument names
+class PrototypeAST {
+  std::string Name;
+  std::vector<std::string> Args;
+public:
+  PrototypeAST(const std::string &name, const std::vector<std::string> &args)
+    : Name(name), Args(args) {}
+};
+
+// FunctionAST - This represents a function call itself.
+class FunctionAST {
+  PrototypeAST *Proto;
+  ExprAST *Body;
+public:
+  FunctionAST(PrototypeAST *proto, ExprAST *body)
+    : Proto(proto), Body(body) {}
+};
+
+// Error - Helper functions for error handling.
+ExprAST *Error(const char *Str) { fprintf(stderr, "ErrorL %s\n", Str); return 0; }
+PrototypeAST *ErrorP(const char *Str) { Error(Str); return 0; }
+FunctionAST *ErrorF(const char *Str) { Error(Str); return 0; }
+
+// ///////////////////
+// Parser Basics
+// ///////////////////
+// CurTok/getNextToken - Provides a simple token buffer.
+static int CurTok;
+static int getNextToken() {
+  return CurTok = gettok();
+}
+
+static ExprAST *ParseExpression();
+
+// numberexpr ::= number
+static ExprAST *ParseNumberExpr() {
+  ExprAST *Result = new NumberExprAST(NumVal);
+  getNextToken(); // consume the number
+  return Result;
+}
+
+// parenexpr ::= '(' expression ')'
+static ExprAST *ParseParenExpr() {
+  getNextToken(); // eat (
+  ExprAST *V = ParseExpression();
+  if (!V) return 0;
+
+  if (CurTok != ')')
+    return Error("expected ')'");
+  getNextToken(); // eat )
+  return V;
+}
+
+// identifierexpr
+static ExprAST *ParseIdentifierExpr() {
+  std::string IdName = IdentifierStr;
+
+  getNextToken(); // eat identifier
+
+  if (CurTok != '(') // simple variable ref
+    return new VariableExprAST(IdName);
+
+  // Call
+  getNextToken(); // eat (
+  std::vector<ExprAST*> Args;
+  if (CurTok != ')') {
+    while (1) {
+      ExprAST *Arg = ParseExpression();
+      if (!Arg) return 0;
+      Args.push_back(Arg);
+
+      if (CurTok == ')') break;
+
+      if (CurTok != ',')
+        return Error("Expected ')' or ',' in argument list");
+      getNextToken();
+    }
+  }
+
+  // Eat the ')'
+  getNextToken();
+
+  return new CallExprAST(IdName, Args);
+}
+
+// primary
+static ExprAST *ParsePrimary() {
+  switch (CurTok) {
+    default: return Error("unknown token when expecting an expression");
+    case tok_identifier: return ParseIdentifierExpr();
+    case tok_number: return ParseNumberExpr();
+    case '(': return ParseParenExpr();
+  }
+}
+
+// BinopPrecedence - Holds precedence for each binary operator
+static std::map<char, int> BinopPrecedence;
+
+// GetTokPrecedence - Gets the precedence of a binary operator token
+static int GetTokPrecedence() {
+  if (!isascii(CurTok))
+    return -1;
+
+  // Make sure it's a declared binop.
+  int TokPrec = BinopPrecedence[CurTok];
+  if (TokPrec <= 0) return -1;
+  return TokPrec;
+}
+
+// binoprhs
+static ExprAST *ParseBinOpRHS(int ExprPrec, ExprAST *LHS) {
+  // If this is a binop, find its precedence.
+  while (1) {
+    int TokPrec = GetTokPrecedence();
+
+    // If this is a binop that binds at least as tightly as the current binop,
+    // consume it, otherwise we are done
+    if (TokPrec < ExprPrec)
+      return LHS;
+
+    // Okay, we know this is a binop
+    int BinOp = CurTok;
+    getNextToken(); // eat binop
+
+    // Parse the primary expression after the binary operator.
+    ExprAST *RHS = ParsePrimary();
+    if (!RHS) return 0;
+
+    // If BinOp binds less tightly with RHS than the operator after, let the
+    // pending operator take RHS as its LHS.
+    int NextPrec = GetTokPrecedence();
+    if (TokPrec < NextPrec) {
+      RHS = ParseBinOpRHS(TokPrec + 1, RHS);
+      if (RHS == 0) return 0;
+    }
+
+    // Merge LHS / RHS
+    LHS = new BinaryExprAST(BinOp, LHS, RHS);
+  }
+
+  return LHS;
+}
+
+// expression
+static ExprAST *ParseExpression() {
+  ExprAST *LHS = ParsePrimary();
+  if (!LHS) return 0;
+
+  return ParseBinOpRHS(0, LHS);
+}
+
+// prototype
+static PrototypeAST *ParsePrototype() {
+  if (CurTok != tok_identifier)
+    return ErrorP("Expected function name in prototype");
+
+  std::string FnName = IdentifierStr;
+  getNextToken();
+
+  if (CurTok != '(')
+    return ErrorP("Expected '(' in prototype");
+
+  // Read the list of argument names.
+  std::vector<std::string> ArgNames;
+  while (getNextToken() == tok_identifier)
+    ArgNames.push_back(IdentifierStr);
+  if (CurTok != ')')
+    return ErrorP("Expected ')' in prototype");
+
+  // success
+  getNextToken(); // eat ')'
+
+  return new PrototypeAST(FnName, ArgNames);
+}
+
+// definition
+static FunctionAST *ParseDefinition() {
+  getNextToken(); // eat def
+  PrototypeAST *Proto = ParsePrototype();
+  if (Proto == 0) return 0;
+
+  if (ExprAST *E = ParseExpression())
+    return new FunctionAST(Proto, E);
+  return 0;
+}
+
+// external
+static PrototypeAST *ParseExtern() {
+  getNextToken(); // eat extern.
+  return ParsePrototype();
+}
+
+// toplevelexpr
+static FunctionAST *ParseTopLevelExpr() {
+  if (ExprAST *E = ParseExpression()) {
+    // Make an anon proto
+    PrototypeAST *Proto = new PrototypeAST("", std::vector<std::string>());
+    return new FunctionAST(Proto, E);
+  }
+  return 0;
+}
+
+//===----------------------------------------------------------------------===//
+// Top-Level parsing
+//===----------------------------------------------------------------------===//
+
+static void HandleDefinition() {
+  if (ParseDefinition()) {
+    fprintf(stderr, "Parsed a function definition.\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
+static void HandleExtern() {
+  if (ParseExtern()) {
+    fprintf(stderr, "Parsed an extern\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
+static void HandleTopLevelExpression() {
+  // Evaluate a top-level expression into an anonymous function.
+  if (ParseTopLevelExpr()) {
+    fprintf(stderr, "Parsed a top-level expr\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
+// top
+static void MainLoop() {
+  while (1) {
+    fprintf(stderr, "ready> ");
+    switch (CurTok) {
+      case tok_eof: return;
+      case ';': getNextToken(); break;
+      case tok_def: HandleDefinition(); break;
+      case tok_extern: HandleExtern(); break;
+      default: HandleTopLevelExpression(); break;
+    }
+  }
+}
+
 int main() {
+  // Install standard binary operators
+  BinopPrecedence['<'] = 10;
+  BinopPrecedence['+'] = 20;
+  BinopPrecedence['-'] = 20;
+  BinopPrecedence['*'] = 40; // highest.
+
+  // Prime the first token
+  fprintf(stderr, "ready> ");
+  getNextToken();
+
+  // Run the main loop
+  MainLoop();
 
   return 0;
 }
