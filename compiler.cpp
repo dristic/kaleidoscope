6698d75df3df9d18a0042355a4b155f6bc1784d5
Dan Ristic <dristic101@gmail.com>
2015-06-22 23:54:47 -0400

    Initial commit.

---
+++ compiler.cpp
@@ -0,0 +1,120 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+// //////////////////////
+// Parser
+// ////////////////////
+
+// The Lexer returns tokens [0-255] if it an unknown character, otherwise one
+// of these for known things.
+enum Token {
+  tok_eof = 1,
+
+  // commands
+  tok_def = -2, tok_extern = -3,
+
+  // primary
+  tok_identifier = -4, tok_number = -5,
+};
+
+static std::string IdentifierStr; // Filled in if tok_identifier
+static double NumVal;             // Filled in if tok_number
+
+// gettok - Return the next token from standard input.
+static int gettok() {
+  static int LastChar = ' ';
+
+  // Skip any whitespace.
+  while (isspace(LastChar))
+    LastChar = getchar();
+
+  if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+    IdentifierStr = LastChar;
+    while (isalnum((LastChar = getchar())))
+      IdentifierStr += LastChar;
+    
+    if (IdentifierStr == "def") return tok_def;
+    if (IdentifierStr == "extern") return tok_extern;
+    return tok_identifier;
+  }
+
+  if (isdigit(LastChar) || LastChar == '.') { // Number: [0-9.]+
+    std::string NumStr;
+
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+
+    NumVal = strtod(NumStr.c_str(), 0);
+    return tok_number;
+  }
+
+  if (LastChar == '#') {
+    // Comment until the end of the line.
+    do LastChar = getchar();
+    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF)
+      return gettok();
+  }
+
+  // Check for EOF. Don't eat the EOF.
+  if (LastChar == EOF)
+    return tok_eof;
+
+  // Otherwise, just return the character as its ascii value
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
+}
+
+// /////////////////////
+// Abstract Syntax Tree
+// ////////////////////
+
+// ExprAST - Base class for all expression nodes.
+class ExprAST {
+  public:
+    virtual ~ExprAST() {}
+};
+
+// NumberExprAST - Expression class for numberic literals "1.0"
+class NumberExprAST : public ExprAST {
+  double Val;
+  public:
+    NumberExprAST(double val) : Val(val) {}
+};
+
+// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST : public ExprAST {
+  std::string Name;
+  public:
+    VariableExprAST(const std::string &name) : Name(name) {}
+};
+
+// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+  char Op;
+  ExprAST *LHS, *RHS;
+  public:
+     BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs)
+      : Op(op), LHS(lhs), RHS(rhs) {}
+};
+
+// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+  std::string Callee;
+  std::vector<ExprAST*> Args;
+  public:
+    CallExprAST(const std::string &callee, std::vector<ExprAST*> &args)
+      : Callee(callee), Args(args) {}
+};
+
+int main() {
+
+  return 0;
+}
